776011a0e93a5cbb485499a54a901901acd1dbf0
Zyox-zSys <zyox.zsys@outlook.com>
2023-06-10 03:46:26 -0600

    BAK: pre header switch to grid

--- script/dark_theme.js
+++ script/dark_theme.js
@@ -1,13 +1,13 @@
 (function() {
-  
   if (!window.matchMedia) return
   
   const darkChk = document.querySelector('#dark_theme'),
     darkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+  if (darkChk.checked && !darkScheme.matches) return
 
   function setTheme() { darkChk.checked = darkScheme.matches }
   
-  setTheme()
   darkScheme.addEventListener('change', setTheme)
-  
+  setTheme()
+  console.debug('dark_theme.js', darkChk.value)
 })()
